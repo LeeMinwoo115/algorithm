b22de83af3f26b540be97c8f01f3d5007d6bb43f
Minwoo Lee <60373193+LeeMinwoo115@users.noreply.github.com>
2026-01-20 19:52:42 +0900

    [level 2] Title: 완전범죄, Time: 0.54 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static final int INF = 1000;
+    public int solution(int[][] info, int n, int m) {
+        int size = info.length;
+        int[][] dp = new int[size+1][m];
+        
+        for (int i=0; i<=size; i++) {
+            for (int j=0; j<m; j++) {
+                dp[i][j] = INF;
+            }
+        }
+        
+        dp[0][0] = 0;
+        
+        for (int i=1; i <= size; i++) {
+            int a = info[i-1][0];
+            int b = info[i-1][1];
+            
+            for (int j=0; j<m; j++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + a);
+                
+                if (j+b < m) {
+                    dp[i][j + b] = Math.min(dp[i][j + b], dp[i-1][j]);
+                }
+            }
+        }
+        
+        int min = INF;
+        for (int j=0; j<m; j++) {
+            min = Math.min(dp[size][j], min);
+        }
+        
+        return min >= n ? -1 : min;
+    }
+}
