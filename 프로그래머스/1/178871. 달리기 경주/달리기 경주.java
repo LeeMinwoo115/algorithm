30716bfc419beeb4b315f65687df93e2845e4a24
Minwoo Lee <60373193+LeeMinwoo115@users.noreply.github.com>
2026-03-07 14:58:34 +0900

    [level 1] Title: 달리기 경주, Time: 289.97 ms, Memory: 376 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<players.length; i++) {
+            map.put(players[i], i);
+        }
+        
+        for (String name: callings) {
+            int idx = map.get(name);
+            
+            String prev = players[idx - 1];
+            
+            players[idx - 1] = name;
+            players[idx] = prev;
+            
+            map.put(name, idx - 1);
+            map.put(prev, idx);
+        }
+        
+        return players;
+    }
+}
