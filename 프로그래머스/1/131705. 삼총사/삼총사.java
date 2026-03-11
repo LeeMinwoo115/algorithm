8adef276965c46903a1223042fb47fe447178eb7
Minwoo Lee <60373193+LeeMinwoo115@users.noreply.github.com>
2026-03-11 20:35:40 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,21 @@
+class Solution {
+    int answer = 0;
+    
+    public int solution(int[] number) {
+        dfs(number, 0, 0, 0);
+        return answer;
+    }
+    
+    private void dfs(int[] number, int start, int depth, int sum) {
+        if (depth == 3) {
+            if (sum == 0) {
+                answer++;
+            }
+            return;
+        }
+        
+        for (int i=start; i<number.length; i++) {
+            dfs(number, i + 1, depth + 1, sum + number[i]);
+        }
+    }
+}
